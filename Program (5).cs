276d6896520ce454574a7df56dab9a8cba03e303
Фадеев Тимур Сергеевич <110234293+fynkyfaya@users.noreply.github.com>
2022-11-10 20:29:45 +0200

    Add files via upload

---
+++ Program (5).cs
@@ -0,0 +1,9 @@
+﻿for (double x = -2; x <= 1; x += 0.5) {
+    double result = 0;
+    if (x > 0) {
+        for (var k = 3; k <= 9; k++) result += Math.Pow(x, -k);
+    } else {
+        result = Math.Pow(2 - x, 2 - x);
+    }
+    Console.WriteLine($"x = {x}, f(x) = {result}");    
+}
